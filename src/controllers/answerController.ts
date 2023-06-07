450774ab9933fd47672056d8b8ffbf232245a182
EmmanunelStocco <emmanuelrolimstocco@gmail.com>
2023-06-07 13:48:48 -0300

    add base

---
+++ src/controllers/answerController.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from 'express'
+import { answerRepository } from '../repositories/answerRepository'
+export class AnswerController {
+    async create(req: Request, res: Response) {
+    }
+}
+
